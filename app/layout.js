3900e50cc1445d73e5ffe995c063deb06abc8071
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-02-09 13:39:06 +0100

    refactor: apply clerk provider for root layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GPTGenius",
@@ -9,10 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-          {children}
-        </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
